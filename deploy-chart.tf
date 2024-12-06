1d1f52a9da2618a8c95895869bb8f0ab12353f85
Alexis Almeida <42656077+AlexisCesar@users.noreply.github.com>
2024-12-06 20:22:27 -0300

    Update helm eks connection (#4)

--- deploy-chart.tf
+++ deploy-chart.tf
@@ -1,6 +1,25 @@
+# provider "helm" {
+#   kubernetes {
+#     config_path = "~/.kube/config"
+#   }
+# }
+
+# resource "null_resource" "update_kubeconfig" {
+#     provisioner "local-exec" {
+#       command = "aws eks update-kubeconfig --region us-east-1 --name eks_lanchonete-do-bairro"
+#     }
+#     depends_on = [ aws_eks_cluster.tc_eks_cluster ]
+# }
+
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = aws_eks_cluster.tc_eks_cluster.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.tc_eks_cluster.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", "eks_lanchonete-do-bairro"]
+      command     = "aws"
+    }
   }
 }
 
